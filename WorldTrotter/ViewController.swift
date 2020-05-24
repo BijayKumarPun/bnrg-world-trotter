0a04aac6af0a1b1bdcb10e471b6f8087144740d7
bijaykumarpun <bijay.workplace@gmail.com>
2020-05-24 09:33:06 +0545

    Create a simple layout with absolute position

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -13,21 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //CGRect that will be frame of a UIView
-        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-        
-        //instance of UIView
-        let firstView = UIView(frame: firstFrame)
-        firstView.backgroundColor = UIColor.blue
-        view.addSubview(firstView)
-        
-        // adding anther view
-        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
-        let secondView = UIView(frame:secondFrame)
-        secondView.backgroundColor = UIColor.green
-        //view.addSubview(secondView)
-        firstView.addSubview(secondView)
+
     }
 }
 
