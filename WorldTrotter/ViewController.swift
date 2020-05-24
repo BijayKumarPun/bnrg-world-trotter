93e2da77f3942c5acedc0eb154a9e30d57c7d1e0
bijaykumarpun <bijay.workplace@gmail.com>
2020-05-24 09:18:45 +0545

    Experiment with view hierarchy

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //CGRect that will be frame of a UIView
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+        
+        //instance of UIView
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blue
+        view.addSubview(firstView)
+        
+        // adding anther view
+        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+        let secondView = UIView(frame:secondFrame)
+        secondView.backgroundColor = UIColor.green
+        //view.addSubview(secondView)
+        firstView.addSubview(secondView)
     }
-
-
 }
 
