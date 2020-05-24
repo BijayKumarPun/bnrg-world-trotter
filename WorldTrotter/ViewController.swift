a38de31e891d54c275b6c4e93a20d7bd58452be6
bijaykumarpun <bijay.workplace@gmail.com>
2020-05-24 08:54:38 +0545

    Init

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Bijay Kumar Pun on 5/24/20.
+//  Copyright © 2020 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
